fd85f989ce7739ba8b86d4b75e383cc5360ca522
bkbartk <bartkock@outlook.com>
2026-02-16 20:02:28 +0100

    Update __init__.py

--- components/motion_detector/__init__.py
+++ components/motion_detector/__init__.py
@@ -6,17 +6,15 @@ from esphome.const import CONF_ID
 motion_detector_ns = cg.esphome_ns.namespace("motion_detector")
 MotionDetector = motion_detector_ns.class_("MotionDetector", binary_sensor.BinarySensor, cg.Component)
 
-CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(MotionDetector),
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(MotionDetector).extend({
     cv.Optional("threshold", default=25): cv.int_,
     cv.Optional("motion_pixels", default=2000): cv.int_,
     cv.Optional("frame_skip", default=5): cv.int_,
 })
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await binary_sensor.new_binary_sensor(config)
     await cg.register_component(var, config)
-    await binary_sensor.register_binary_sensor(var, config)
 
     cg.add(var.threshold.set(config["threshold"]))
     cg.add(var.motion_pixels.set(config["motion_pixels"]))
